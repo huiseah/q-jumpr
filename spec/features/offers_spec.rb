ef55b7e5fdab97ad4d72cd6eaa55e1490d05e80e
hui <hui@hui-AO725.(none)>
2013-08-09 09:56:01 +1000

    present

--- spec/features/offers_spec.rb
+++ spec/features/offers_spec.rb
@@ -51,29 +51,29 @@ describe 'offers' do
     end
   end
 
-  # describe 'POST /offer/new' do
-  #   it 'creates a new offer', :js => true do
-  #     visit root_path
-  #     login
-  #     click_link('Kill a Queue')
-  #     page.should have_button('Create Offer')
-  #     fill_in('name', :with => 'Hoyts')
-  #     fill_in('address', :with => 'Broadway')
-  #     fill_in('date', :with => Time.now)
-  #     fill_in('price', :with => 50)
-  #     click_button('Create Offer')
-  #     page.should_not have_button('Create Offer')
-  #     expect(Offer.first.price).to eq 50
-  #   end
-  # end
+  describe 'POST /offer/new' do
+    it 'creates a new offer', :js => true do
+      visit root_path
+      login
+      click_link('Kill a Queue')
+      page.should have_button('Create Offer')
+      fill_in('name', :with => 'Hoyts')
+      fill_in('address', :with => 'Broadway')
+      fill_in('date', :with => Time.now)
+      fill_in('price', :with => 50)
+      click_button('Create Offer')
+      page.should_not have_button('Create Offer')
+      expect(Offer.first.price).to eq 50
+    end
+  end
 
 end
 
-# def login
-#   visit root_path
-#   click_link('Login')
-#   page.should have_text('Login')
-#   fill_in('email', :with => 'bob@gmail.com')
-#   fill_in('password', :with => 'a')
-#   click_button('Login')
-# end
+def login
+  visit root_path
+  click_link('Login')
+  page.should have_text('Login')
+  fill_in('email', :with => 'bob@gmail.com')
+  fill_in('password', :with => 'a')
+  click_button('Login')
+end
